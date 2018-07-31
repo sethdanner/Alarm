63a36cf9b9ad7e114afbe3f3a8b8e60e7fb66b16
Seth <sethdanner@gmail.com>
2018-07-31 17:27:12 -0600

    Added view controllers.

---
+++ Alarm/ViewControllers/AlarmDetailTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AlarmDetailTableViewController.swift
+//  Alarm
+//
+//  Created by Seth Danner on 7/31/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import UIKit
+
+class AlarmDetailTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+ 
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
